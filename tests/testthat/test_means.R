c96f1e376e6cc5464b11215818895834fcd3ade4
Kellie Ottoboni <kellieotto@berkeley.edu>
2015-06-03 17:26:48 -0700

    TST: add tests for t2p, one_sample, two_sample

---
+++ tests/testthat/test_means.R
@@ -0,0 +1,33 @@
+set.seed(55)
+
+context("One sample")
+test_that("bad inputs",{
+  expect_error(one_sample("k"), "numeric") 
+  expect_error(one_sample(data.frame(1:5)), "vector") 
+})
+
+test_that("IPAT example from chapter 1", {
+  data(ipat)
+  d <- ipat$YA - ipat$YB
+  distr <- one_sample(d)
+  expect_equal(min(distr), -2.6)
+  expect_equal(max(distr), 2.6)
+  expect_equal(median(distr), 0)
+})
+
+context("Two sample")
+test_that("bad inputs",{
+  expect_error(two_sample("k",4), "numeric") 
+  expect_error(two_sample(data.frame(1:5), 40), "vector") 
+})
+
+test_that("jobs example from chapter 1", {
+  data(job)
+  group1 <- job[job$Y == 1,"X"]
+  group2 <- job[job$Y == 2,"X"]  
+  observed <- mean(group1) - mean(group2)
+  distr <- two_sample(group1, group2)
+  expect_equal(min(distr), -16.875)
+  expect_equal(max(distr), 18+1/3)
+  expect_equal(median(distr), 0)
+})
--- tests/testthat/test_t2p.R
+++ tests/testthat/test_t2p.R
@@ -1,8 +1,16 @@
 context("Bad inputs")
 
-context("Correct p-values")
-test_that("p-values are correct", {
-  expect_equal(t2p(5, -10:10, "upper"), c("pupper" = 6/21))
-  expect_equal(t2p(5, -10:10, "lower"), c("plower" = 16/21))
+context("Correct p-values from t2p")
+test_that("p-values from toy example", {
+  expect_equal(t2p(5, -10:10, "greater"), c("pupper" = 6/21))
+  expect_equal(t2p(5, -10:10, "less"), c("plower" = 16/21))
   expect_equal(t2p(5, -10:10, "two-sided"), c("pboth" = 12/21))
+})
+
+test_that("p-values for IPAT data",{
+  data(ipat)
+  d <- ipat$YA-ipat$YB
+  distr <- one_sample(d)
+  pval <- t2p(mean(d), distr)
+  expect_equal(pval, c("pupper"=0,"plower"=1,"pboth"=0))
 })
