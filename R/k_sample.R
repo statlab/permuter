1881d7c012570d737a199d77471c25c63228d45f
Kellie Ottoboni <kellieotto@berkeley.edu>
2015-06-06 22:15:05 -0700

    ENH: add permute_within_groups and two-way anova option to k_sample

--- R/k_sample.R
+++ R/k_sample.R
@@ -1,24 +1,76 @@
+#' Permute within groups
+#'
+#' Permute values within groups
+#' @param x     vector of responses
+#' @param group vector of group assignments
+#' @return a vector of length(x)
+permute_within_groups <- function(x, group){
+  groups <- unique(group)
+  permuted <- x
+  for(gg in groups){
+    permuted[group == gg] <- sample(x[group == gg])
+  }
+  return(permuted)
+}
+
+
+
 #' k-sample permutation test for equality of distributions
 #'
 #' @inheritParams one_sample
-#' @param group Vector of group memberships
-#' 
+#' @param group  Vector of group memberships, exchangeable under the null
+#' @param group2 Vector of secondary group memberships (optional)
+#' @param stat  Test statistic: either "oneway_anova" (default) or "twoway_anova"
 #' @return A vector of length `reps` containing the permutation distribution
-k_sample <- function(x, group, reps=1000){
+k_sample <- function(x, group, group2=NULL, reps=1000, stat = "oneway_anova"){
   n <- table(group)
-  anova <- function(group){
+  n2 <- table(group2)
+  
+  ## check that if twoway_anova is specified, then group2 != NULL
+  
+  oneway_anova <- function(group){
     k <- unique(group)
     xbar <- rep(NA, length(k))
     for(gg in k){
-      xbar[gg] <- mean(X[group == gg], na.rm=T)
+      xbar[gg] <- mean(X[group == gg])
     }
-    tst <- sum(xbar^2*n) # The permutation ANOVA statistic: sum_{groups} n_j*(mean(X_j))^2
+    tst <- sum(xbar^2*n) # The permutation ANOVA statistic: sum_{j in groups} n_j*(mean(X_j))^2
     return(tst)
+  }
+  twoway_anova <- function(group){
+    k <- unique(group)
+    xbar <- rep(NA, length(k))
+    for(gg in k){
+      xbar[gg] <- mean(X[group == gg])
+    }
+    xbarbar <- mean(xbar)
+    tst <- sum((xbar - xbarbar)^2) # The permutation two-way ANOVA stat is SSB=sum_{j in groups} (mean(X_j) - grand mean)^2
+    return(tst)
+  }
+  test_stats <- list(oneway_anova, twoway_anova)
+  names(test_stats) <- c("oneway_anova", "twoway_anova")
+  tst <- test_stats[[stat]]
+  
+  if(stat %in% c("oneway_anova")){
+    perm_method <- function(g) sample(g)
+  }else{
+    perm_method <- function(g) permute_within_groups(g, group2)
   }
   
   distr <- replicate(reps, {
-              perm <- sample(group)
-              anova(perm)
+              perm <- perm_method(group)
+              tst(perm)
             })
   return(distr)
+}
+
+#TODO -- constrained permutation
+#' k-sample test for equality of distributions
+#'
+#' @inheritParams k_sample
+#' @param stat  either "ks" (Kolmogorov-Smirnov, default) or "ad" (Anderson-Darling)
+#' 
+#' @return A vector of length `reps` containing the permutation distribution
+goodness_of_fit <- function(x, group, reps=1000, stat = "ks"){
+  return(NULL)
 }
