2640d11d15fbfa3c98544f8c8bdce4c619103122
Kellie Ottoboni <kellieotto@berkeley.edu>
2015-06-15 22:25:56 -0700

    TST: add tests for fwe_minp

---
+++ tests/testthat/test_fwe_minp.R
@@ -0,0 +1,16 @@
+set.seed(222)
+pval <- seq(0.05, 0.9, length.out = 5)
+distr <- matrix(runif(5*100, min=0, max=10), ncol = 5)
+
+context("FWE MinP")
+test_that("bad inputs",{
+  expect_error(npc(pval, distr, combine = "something else"), "combining function") 
+  expect_error(npc(pval[1], distr[,1]), "Nothing") 
+  expect_error(npc(pval, distr[,-1]), "Different number")
+})
+
+test_that("p-values from toy example", {
+  expect_equal(fwe_minp(pval, distr, "fisher"), c(0.35, 0.73, 0.84, 0.9, 0.9))
+  expect_equal(fwe_minp(pval, distr, "liptak"), c(0.42, 0.67, 0.78, 0.9, 0.9))
+  expect_equal(fwe_minp(pval, distr, "tippett"), c(0.25, 0.68, 0.84, 0.9, 0.9))
+})
