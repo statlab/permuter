005075926f6c309be81aafa369ec86158e7d47f7
Kellie Ottoboni <kellieotto@berkeley.edu>
2016-04-25 14:17:19 -0700

    ENH: Add IRR npc function

--- R/irr.R
+++ R/irr.R
@@ -20,7 +20,7 @@ compute_irr_ts <- function(ratings) {
 #' a matrix of ratings ``ratings``
 #' 
 #'         
-#' If \code{obs_ts} is not \code{None}, computes the reference value of the test
+#' If \code{obs_ts} is not null, computes the reference value of the test
 #' statistic before the first permutation. Otherwise, uses the value
 #' \code{obs_ts} for comparison.
 #' 
@@ -75,3 +75,63 @@ irr_ts_distribution <- function(ratings, obs_ts = NULL, num_perm = 10000, keep_d
               "pvalue" = (geq/num_perm), "dist" = dist)
   return(res)
 }
+
+
+#' Combine the IRR statistics across strata
+#' 
+#' Use NPC to combine the IRR statistics across strata.
+#' 
+#'     Simulates the permutation distribution of the combined NPC test
+#'     statistic for S matrices of ratings \code{obs_ts} corresponding to
+#'     S strata. The distribution comes from applying \code{obs_ts}
+#'     to each of the S strata.
+#'     
+#'     If \code{obs_ts} is not null, computes the reference value of the test
+#'     statistic before the first permutation. Otherwise, uses the value
+#'     \code{obs_ts} for comparison.
+#'     
+#'     If \code{keep_dist}, return the distribution of values of the test
+#'     statistic; otherwise, return only the number of permutations
+#'     for which the value of the irr test statistic is at least
+#'     as large as \code{obs_ts}.
+#'     
+#' @param perm_distr Input matrix of dimension [B, S]
+#'        Column s is the permutation distribution of \eqn{rho_s}, for \eqn{s = 1, \dots, S}.
+#' @param size Vector of S sample sizes. 
+#'        Entry s is the number of items \eqn{N_s} in stratum s.
+#' @param obs_ts Optional: the NPC test statistic.
+#'        If NULL, the NPC test statistic is computed using \code{pvalues}.
+#' @param pvalues Optional: a vector of S IRR p-values for each stratum.
+#'        Entry s is the p-value for \eqn{rho_s}, the concordance in stratum s.
+#' 
+#' @return A list containing:
+#' \itemize{
+#' \item{obs_npc: observed value of the test statistic for the input data, or the input value of \code{obs_ts} if it was given as input}
+#' \item{geq: integer number of iterations for which the test statistic was greater than or equal to \code{obs_ts}}
+#' \item{num_perm: number of permutations}
+#' \item{pvalue: geq/num_perm}
+#' \item{dist: if \code{keep_dist}, the array of values of the irr test statistic from the \code{num_perm} iterations. Otherwise, NULL.}
+#' }
+#' 
+irr_npc_distribution <- function(perm_distr, size, obs_ts = NULL, pvalues = NULL){
+  if(is.null(obs_ts) & is.null(pvalues)){
+    stop("You must input either obs_ts or pvalues")
+  }
+  
+  B <- nrow(perm_distr)
+  S <- ncol(perm_distr)
+  combine_func <- function(p){inverse_n_weight(p, size)}
+  
+  if(is.null(pvalues)){
+    pvalues <- rep(NA, S)
+    for(j in seq_len(S)){
+      pvalues[j] <- mean(perm_distr[,j] >= obs_ts[j])
+    }
+  }
+  
+  obs_npc <- combine_func(pvalues)
+  res <- npc(pvalues, perm_distr, combine_func, alternatives = "greater")
+  return(list("obs_npc" = obs_npc,
+              "pvalue"  = res,
+              "num_perm"= B))
+} 
