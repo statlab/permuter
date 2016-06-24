29077cd12c100b7cef2e9e32eca3aa194c0714ff
Kellie Ottoboni <kellieotto@berkeley.edu>
2016-06-24 10:26:55 -0700

    TST: Add tests for stratified_two_sample

--- tests/testthat/test_means.R
+++ tests/testthat/test_means.R
@@ -31,3 +31,40 @@ test_that("jobs example from chapter 1", {
     expect_equal(max(distr), 18 + 1/3)
     expect_equal(median(distr), 0)
 }) 
+
+context("Stratified two sample")
+test_that("bad inputs", {
+  expect_error(stratified_two_sample(4, "k", 5), "numeric")
+  expect_error(stratified_two_sample(data.frame(1:5), 20, 40), "vector")
+  expect_error(stratified_two_sample(5, 20, 40, stat="lame"), "dictionary of stats")
+})
+
+test_that("simple example", {
+  group <- c(rep(1, 4), rep(2, 4))
+  stratum <- c("b", rep("a", 4), rep("b", 3))
+  response <- 1:8
+  
+  set.seed(55)
+  distr <- stratified_two_sample(group, response, stratum)
+  expect_equal(min(distr), -3.5)
+  expect_equal(median(distr), -0.3)
+  expect_equal(max(distr), 3.3)
+  
+  set.seed(55)
+  distr <- stratified_two_sample(group, response, stratum, stat = "mean_within_strata")
+  expect_equal(min(distr), 1.333, tolerance = 1e-3)
+  expect_equal(median(distr), 4.0)
+  expect_equal(max(distr), 9.333, tolerance = 1e-3)
+})
+
+test_that("example from permute",{
+  stratum <- rep(1:3, each=10)
+  group <- rep( rep(1:2, each=5), 3)
+  response <- rep(0, length(group))
+  response[c(1, 2, 4, 10, 11, 12, 19, 20, 21)] <- 1
+  
+  set.seed(55)
+  res <- stratified_two_sample(group, response, stratum, reps=10000)
+  expect_equal(mean(response[group==1]) - mean(response[group==2]), 0.2)
+  expect_equal(c("pupper"=0.1523), t2p(0.2, res, "greater"))
+})
