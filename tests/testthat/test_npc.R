9e1030573faf1b3b83c265a56d6299c2b1efbeac
Kellie Ottoboni <kellieotto@berkeley.edu>
2016-11-07 10:24:10 +0100

    TST: modify tests of NPC. For the most part, results are the same. I had to change p-values input to statistics, and I based the conversion on the "greater than" alternative. This yields similar results for NPC using "greater than", but different p-values for NPC with other alternatives.

--- tests/testthat/test_npc.R
+++ tests/testthat/test_npc.R
@@ -1,25 +1,26 @@
 set.seed(55)
 pval <- seq(0.05, 0.9, length.out = 5)
+tst <- c(9.6, 6.8, 5.8, 2.5, 1)
 distr <- matrix(runif(5 * 100, min = 0, max = 10), ncol = 5)
 
 context("NPC")
 test_that("bad inputs", {
-    expect_error(npc(pval, distr, combine = "something else"), "combining function")
-    expect_error(npc(pval[1], distr[, 1]), "Nothing")
-    expect_error(npc(pval, distr[, -1]), "Different number")
-    expect_error(npc(pval, distr, alternatives = c("greater", "greater")), "Bad alternatives")
+    expect_error(npc(tst, distr, combine = "something else"), "combining function")
+    expect_error(npc(tst[1], distr[, 1]), "Nothing")
+    expect_error(npc(tst, distr[, -1]), "Different number")
+    expect_error(npc(tst, distr, alternatives = c("greater", "greater")), "Bad alternatives")
 })
 
 test_that("p-values from toy example", {
-    expect_equal(npc(pval, distr, "fisher", "greater"), 0.32)
-    expect_equal(npc(pval, distr, "fisher", "less"), 0.31)
-    expect_equal(npc(pval, distr, "fisher", "two-sided"), 0.23)
-    expect_equal(npc(pval, distr, "liptak"), 0.34)
-    expect_equal(npc(pval, distr, "tippett"), 0.25)
-    expect_equal(npc(pval, distr, combine = function(p) inverse_n_weight(p, rep(1, 
-        length(pval)))), 0.39)
-    expect_equal(npc(pval, distr, alternatives = c("less", "greater", "less", "greater", 
-        "two-sided")), 0.34)
+    expect_equal(npc(tst, distr, "fisher", "greater"), 0.27)
+    expect_equal(npc(tst, distr, "fisher", "less"), 0.57)
+    expect_equal(npc(tst, distr, "fisher", "two-sided"), 0.27)
+    expect_equal(npc(tst, distr, "liptak"), 0.32)
+    expect_equal(npc(tst, distr, "tippett"), 0.15)
+    expect_equal(npc(tst, distr, combine = function(p) inverse_n_weight(p, rep(1, 
+        length(tst)))), 0.43)
+    expect_equal(npc(tst, distr, alternatives = c("less", "greater", "less", "greater", 
+        "two-sided")), 0.62)
 })
 
 context("Combining functions")
